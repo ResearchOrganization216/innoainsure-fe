3e2932bfe8a21a710193b3a57d9bb3978a1ff350
it21257568 <it21257568@my.sliit.lk>
2025-03-17 12:08:29 +0530

    Fixes type errors

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -13,7 +13,7 @@ export function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [, setIsLoading] = useState(false);
 
   // Zustand store action
   const storeUser = useAuthStore((state) => state.storeUser);
